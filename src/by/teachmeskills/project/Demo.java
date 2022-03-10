2463c839e58a4e1c883c721a2a2d8b5f0759fa43
Roman Tkachyov <tkachyovroman@gmail.com>
2022-03-10 21:16:06 +0300

    add demo to develop branch

--- src/by/teachmeskills/project/Demo.java
+++ src/by/teachmeskills/project/Demo.java
@@ -2,6 +2,6 @@ package by.teachmeskills.project;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Buy2, palls");
+        System.out.println("Buy22, palls");
     }
 }
