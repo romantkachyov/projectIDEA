80e673ab482490d3c361ef641b623da1afe6f19b
tkachyovroman <tkachyovroman@gmail.com>
2022-03-12 16:40:23 +0300

    new file name

--- src/by/teachmeskills/project/Demo.java
+++ src/by/teachmeskills/project/Demo.java
@@ -2,7 +2,7 @@ package by.teachmeskills.project;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("000000, palls");
+        System.out.println("651+9651, palls");
 
     }
 }
