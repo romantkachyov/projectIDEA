922ce7bded3b7de8b3e59d4ebfbce1e5df235674
Roman Tkachyov <100864882+romantkachyov@users.noreply.github.com>
2022-03-12 16:34:44 +0300

    Update Demo.java

--- src/by/teachmeskills/project/Demo.java
+++ src/by/teachmeskills/project/Demo.java
@@ -2,7 +2,7 @@ package by.teachmeskills.project;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Buy212, palls");
+        System.out.println("945, palls");
 
     }
 }
